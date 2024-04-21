74d1f89d6870fd695a703be0d5ac2c5617a1290b
mohan-jayabalan <mohanstc@gmail.com>
2024-04-21 11:26:29 +0530

    TestSuitesCollection and PetStore Testcase added..

---
+++ Test Suites/PetStore/TS_Login.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>61d81d98-9d0d-4e87-aa0b-996b322ced58</testSuiteGuid>
+   <testCaseLink>
+      <guid>7d5e0a91-35f3-4bcc-ad6b-829b0e572946</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/PetStore/Login</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
